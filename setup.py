91e822170082c70e7cd3ffb3e3d9d8374c596e5f
root <chandbibak09@gmail.com>
2024-09-29 20:42:34 +0545

    Modified test and setup

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
-from setuptools import setup
+from setuptools import setup,find_packages
 
 setup(
    name='news_cluster',
    version='1.0',
    description='Clusteirng nepali news based on the incidents',
    packages=find_packages(),
-   install_requires=['pandas','numpy','scikit-learn']
+   install_requires=['pandas','numpy','scikit-learn'],
+   test = ['news_cluster.py'],
 )
--- test/news_cluster.py
+++ test/news_cluster.py
@@ -1,4 +1,5 @@
 from package.cluster_news import cluster
 
+
 json_file = "./stemmed-4687326bb4b23cfae6009636d83fbc8883556e5c.json"
 cluster(json_file)
